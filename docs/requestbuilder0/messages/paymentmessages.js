f43e4cc01d29ef9d0bb8f54ff26100372916d12f
JorgeR1986 <81313045+JorgeR1986@users.noreply.github.com>
2021-04-13 16:16:24 -0500

    publication for today

--- docs/requestbuilder0/messages/paymentmessages.js
+++
@@ -1,8 +0,0 @@
-function checkPaymentMessages() {
-
-  // TO DO when there are messages to show.
-  // for (var property of bodyBuilder.providers) {
-  // }
-
-
-}
--- public/requestbuilder/bbconfiglookups.js
+++
@@ -1,7 +0,0 @@
-$.getJSON("/requestbuilder/configlookups/_unsupportedrequests.json", function(json) {
-    bodyBuilder.unsupportedRequests = json;
-});
-
-$.getJSON("/requestbuilder/configlookups/supportedThreedDSProviders.json", function(json) {
-    bodyBuilder.supportedTreeDsProviders = json;
-});
